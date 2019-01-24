a8568172aba8b97efb63dfadff1b9b579d64bb8d
Earl Debicki <bartosz.debicki@uxpin.com>
2019-01-24 17:33:18 +0100

    add jsx preset for button

---
+++ src/Button/presets/0-default.jsx
@@ -0,0 +1,16 @@
+import React from 'React';
+import Button from '../Button';
+import Icon from '../../Icon/Icon';
+
+export default (
+  <Button
+    rootId="button1"
+    stretched
+    type="error"
+    mode="filled"
+    icon={<Icon size="s" icon="TickerSvg" />}
+    size="s"
+  >
+    Merge!
+  </Button>
+);
