7e49250189134e11daf97580cba3913a6798fa49
Earl Debicki <bartosz.debicki@uxpin.com>
2019-01-25 14:48:54 +0100

    add jsx presets for selec component

---
+++ src/Select/Select/presets/0-default.jsx
@@ -0,0 +1,11 @@
+import React from 'React';
+import Select from '../Select';
+import SelectItem from '../../SelectItem/SelectItem';
+
+export default (
+  <Select uxpId="select1">
+    <SelectItem uxpId="select2">Option1</SelectItem>
+    <SelectItem uxpId="select3">Option2</SelectItem>
+    <SelectItem uxpId="select4">Option3</SelectItem>
+  </Select>
+);
