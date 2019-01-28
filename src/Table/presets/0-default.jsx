c6acacbcf92b5f2add892073acccd0baa55e36cf
Earl Debicki <bartosz.debicki@uxpin.com>
2019-01-28 11:39:34 +0100

    polish table data in preset

--- src/Table/presets/0-default.jsx
+++ src/Table/presets/0-default.jsx
@@ -5,51 +5,51 @@ const tableData = {
   header: ['band', 'singer', 'rhythm guitar', 'lead guitar', 'bass', 'drums', 'keyboard'],
   body: [
     {
-      'band': 'metallica',
-      'singer': 'james hetfield',
+      band: 'metallica',
+      singer: 'james hetfield',
       'rhythm guitar': 'james hetfield',
       'lead guitar': 'kirk hammet',
-      'bass': 'robert trujillo',
-      'drums': 'lars ulrich'
+      bass: 'robert trujillo',
+      drums: 'lars ulrich'
     },
     {
-      'band': 'slayer',
-      'singer': 'tom araya',
+      band: 'slayer',
+      singer: 'tom araya',
       'rhythm guitar': 'kerry king',
       'lead guitar': 'gary holt',
-      'bass': 'tom araya',
-      'drums': 'paul bostaph'
+      bass: 'tom araya',
+      drums: 'paul bostaph'
     },
     {
-      'band': 'black sabbath',
-      'singer': 'ozzy osbourne',
+      band: 'black sabbath',
+      singer: 'ozzy osbourne',
       'lead guitar': 'tommy iommi',
-      'bass': 'geezer butler',
-      'drums': 'bill ward'
+      bass: 'geezer butler',
+      drums: 'bill ward'
     },
     {
-      'band': 'queen',
-      'singer': 'freddy mercury',
+      band: 'queen',
+      singer: 'freddy mercury',
       'lead guitar': 'brian may',
-      'bass': 'john deacon',
-      'drums': 'roger taylor',
-      'keyboard': 'freddy mercury'
+      bass: 'john deacon',
+      drums: 'roger taylor',
+      keyboard: 'freddy mercury'
     },
     {
-      'band': 'led zeppelin',
-      'singer': 'robert plant',
+      band: 'led zeppelin',
+      singer: 'robert plant',
       'lead guitar': 'jimmy page',
-      'bass': 'john paul johns',
-      'drums': 'bonzo bonham',
-      'keyboard': 'john paul johns'
+      bass: 'john paul johns',
+      drums: 'bonzo bonham',
+      keyboard: 'john paul johns'
     },
     {
-      'band': 'deep purple',
-      'singer': 'ian gillan',
+      band: 'deep purple',
+      singer: 'ian gillan',
       'lead guitar': 'ritchie blackmore',
-      'bass': 'roger glover',
-      'drums': 'ian paice',
-      'keyboard': 'jon lord'
+      bass: 'roger glover',
+      drums: 'ian paice',
+      keyboard: 'jon lord'
     }
   ]
 };
