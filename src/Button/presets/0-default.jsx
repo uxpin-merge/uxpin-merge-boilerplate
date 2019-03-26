145baf1229d86100571ca938cb6395cc7a13ab75
Earl Debicki <bartosz.debicki@uxpin.com>
2019-03-26 11:55:27 +0100

    update button preset by new jsx syntax

--- src/Button/presets/0-default.jsx
+++ src/Button/presets/0-default.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Button from '../Button';
+import Icon from '../../Icon/Icon';
 
 export default (
   <Button
-    icon={'<Icon icon=\'TickerSvg\' size=\'s\' />'}
+    icon={<Icon icon="TickerSvg" size="s" uxpId="button2" />}
     mode="filled"
     size="s"
     stretched
