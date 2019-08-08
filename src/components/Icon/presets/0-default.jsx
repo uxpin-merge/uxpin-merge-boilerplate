5e534ec0210b5569c61864b47809d435a558b016
Radek Taraszka <radoslaw.taraszka@uxpin.com>
2019-08-08 16:29:25 +0200

    merged changes

--- src/components/Icon/presets/0-default.jsx
+++ src/components/Icon/presets/0-default.jsx
@@ -3,15 +3,9 @@ import Icon from '../Icon';
 
 export default (
   <Icon
-<<<<<<< HEAD:src/components/Icon/presets/0-default.jsx
-    icon="ChangelogSvg"
-    size="xxl"
-    uxpId="icon1"
-=======
     icon="CalendarSvg"
     color="primary"
     size="xxl"
     uxpId="icon_svg"
->>>>>>> master:src/Icon/presets/0-default.jsx
   />
 );
