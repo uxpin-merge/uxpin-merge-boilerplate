c8a9a9027709e350383860c1fb587e791e18b1fe
Radek Taraszka <radoslaw.taraszka@uxpin.com>
2019-08-21 16:00:56 +0200

    Link component added

--- uxpin.config.js
+++ uxpin.config.js
@@ -3,10 +3,12 @@ module.exports = {
     categories: [
       {
         name: 'General',
+        // eslint-disable-next-line sort-keys
         include: [
           'src/components/Button/Button.js',
           'src/components/Greeting/Greeting.js',
           'src/components/Icon/Icon.js',
+          'src/components/Link/Link.js',
           'src/components/Select/Select.js',
           'src/components/Select/components/SelectItem/SelectItem.js',
           'src/components/Table/Table.js',
@@ -14,6 +16,7 @@ module.exports = {
       },
       {
         name: 'Charts',
+        // eslint-disable-next-line sort-keys
         include: [
           'src/components/Charts/**/*.js',
           '!src/components/Charts/**/*.styles.js',
