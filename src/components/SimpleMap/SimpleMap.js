d99ad284249e44509683a04c29c57588b085298a
Radek Taraszka <radoslaw.taraszka@uxpin.com>
2020-03-19 10:56:56 +0100

    Credit Card & TrendLine added

--- src/components/SimpleMap/SimpleMap.js
+++ src/components/SimpleMap/SimpleMap.js
@@ -2,31 +2,40 @@
 import React from 'react';
 import GoogleMapReact from 'google-maps-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// eslint-disable-next-line react/prop-types
+const AnyReactComponent = ({ text }) => (
+  <div style={{
+    color: 'white',
+    background: 'grey',
+    padding: '15px 10px',
+    display: 'inline-flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '100%',
+    transform: 'translate(-50%, -50%)',
+  }}>
+    {text}
+  </div>
+);
 
-class SimpleMap extends Component {
+class SimpleMap extends React.Component {
   static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33,
-    },
+    center: { lat: 59.95, lng: 30.33 },
     zoom: 11,
   };
 
   render() {
     return (
-      <div style={{ height: '400px', width: '300px' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: AIzaSyD299I8UJkhkEGd - QPlejRoQ5BcacAvoJ4 }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}>
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
+      <GoogleMapReact
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}>
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="Kreyser Avrora"
+        />
+      </GoogleMapReact>
     );
   }
 }
