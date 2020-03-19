1c9a13be6b62b82008524e3fd27b566ac177c0b4
Radek Taraszka <radoslaw.taraszka@uxpin.com>
2020-03-19 11:04:31 +0100

    Credit Card & TrendLine added

--- src/components/TrendLine/helpers/misc.helpers.js
+++ src/components/TrendLine/helpers/misc.helpers.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/prefer-default-export */
 // eslint-disable-next-line no-restricted-properties
 // eslint-disable-next-line import/prefer-default-export
+// eslint-disable-next-line no-restricted-properties
 export const generateId = () => Math.round(Math.random() * Math.pow(10, 16));
