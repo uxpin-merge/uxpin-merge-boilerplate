a61a8501858c5f2b302e52f67ec89aacd4f0dbf9
rtaraszka <radoslaw.taraszka@gmail.com>
2020-03-23 15:07:18 +0100

    Added MediaPlayer

---
+++ src/components/MediaPlayer/presets/0-default.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MediaPlayer from '../MediaPlayer';
+
+export default (
+  <MediaPlayer />
+);
--- uxpin.config.js
+++ uxpin.config.js
@@ -10,6 +10,7 @@ module.exports = {
           'src/components/CreditCard/CreditCard.jsx',
           'src/components/Greeting/Greeting.js',
           'src/components/Icon/Icon.js',
+          'src/components/MediaPlayer/MediaPlayer.js',
           'src/components/Select/Select.js',
           'src/components/Select/components/SelectItem/SelectItem.js',
           'src/components/Table/Table.js',
