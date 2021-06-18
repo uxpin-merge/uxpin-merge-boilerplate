6f351ed2cd4333577839efc78a6a590eee2d20f1
sam moore <swmoore84@gmail.com>
2021-06-18 17:21:32 +0900

    add text field

--- uxpin.config.js
+++ uxpin.config.js
@@ -13,6 +13,7 @@ module.exports = {
           'src/components/FormaValidationMessage/FormaValidationMessage.jsx',
           'src/components/Workbench/Workbench.jsx',
           'src/components/WorkbenchHeader/WorkbenchHeader.jsx',
+          'src/components/TextField/TextField.jsx',
 
         ],
       },
