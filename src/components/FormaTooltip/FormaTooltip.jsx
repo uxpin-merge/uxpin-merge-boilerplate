3e1a4d6be01f62049b123fb8aaa9bfada1bedf4f
Jack Behar <jack@uxpin.com>
2021-06-22 10:50:29 -0600

    Tooltip: fixed jump and display issues

--- src/components/FormaTooltip/FormaTooltip.jsx
+++ src/components/FormaTooltip/FormaTooltip.jsx
@@ -2,15 +2,17 @@ import * as PropTypes from 'prop-types';
 import * as React from 'react';
 import { Tooltip } from '@contentful/forma-36-react-components';
 
-/**
- * @uxpinwrappers
- * ---NonResizableWrapper
- */
-const FormaTooltip = (props) => (
-  <Tooltip {...props} >
-    {props.children}
-  </Tooltip>
-);
+
+const FormaTooltip = (props) => {
+  const { uxpinRef, ...other } = props;
+  return (
+    <div ref={uxpinRef}>
+      <Tooltip  {...other}  >
+        {props.children}
+      </Tooltip>
+    </div>
+  )
+}
 
 
 FormaTooltip.propTypes = {
@@ -18,44 +20,72 @@ FormaTooltip.propTypes = {
    * The content to be displayed in the tooltip  
    */
   content: PropTypes.node,
+
+  /**
+   * It sets the "preferred" position of the Tooltip  
+   */
+  place: PropTypes.oneOf(["left", "right", "auto", "top", "bottom"]),
+
+  /**
+   * The max-width of the tooltip  
+   */
+  maxWidth: PropTypes.number,
+
+  /**
+   * It sets a delay period for the Tooltip  
+   */
+  hideDelay: PropTypes.number,
+
+  /**
+   * If checked tooltip will be visible (requires browser refresh)  
+   */
+  isVisible: PropTypes.bool,
+
+  /**
+  * Function that will be called when target gets focused  
+  */
+  onFocus: PropTypes.func,
+
+  /**
+  * Function that will be called when target gets blurred  
+  */
+  onBlur: PropTypes.func,
+
+  /**
+    * Function that will be called when the user move the mouse out of the target  
+    */
+  onMouseLeave: PropTypes.func,
+
+  /**
+    * Function that will be called when the user move the mouse over of the target  
+    */
+  onMouseOver: PropTypes.func,
+
   /** 
   * @uxpinignoreprop
   */
   children: PropTypes.node,
+
   /**
   * @uxpinignoreprop
   */
   containerElement: PropTypes.element,
 
-  isVisible: PropTypes.bool,
-
-  maxWidth: PropTypes.number,
-
-  hideDelay: PropTypes.number,
-
-  onFocus: PropTypes.func,
-
-  onMouseLeave: PropTypes.func,
-
-  onMouseOver: PropTypes.func,
-
-  place: PropTypes.oneOf(["left", "right", "auto", "top", "bottom"]),
-
+  /**
+* @uxpinignoreprop
+*/
   targetWrapperClassName: PropTypes.string,
 
+  /**
+  * @uxpinignoreprop
+  */
   usePortal: PropTypes.bool,
+
 };
 
-
-
 FormaTooltip.defaultProps = {
-content: "Hi I am a tooltip",
-usePortal: true,
-isVisible: false
-// children: "Children Text",
-// isVisible: true,
-// maxWidth: 360,
-// place: "top"
+  content: "Hi I am a tooltip",
+  place: "top",
 };
 
 export { FormaTooltip as default };
--- src/components/FormaTooltip/presets/0-default.jsx
+++ src/components/FormaTooltip/presets/0-default.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import FormaTooltip from '../FormaTooltip';
 import FormaTextLink from '../../FormaTextLink/FormaTextLink';
 
-
 export default (
   <FormaTooltip uxpId="FormaTextLink-1">
     <FormaTextLink text="Hover me to see tooltip" uxpId="FormaTextLink-Child-1" />
