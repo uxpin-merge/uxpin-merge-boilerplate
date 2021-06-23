8c44292979f4d43e50c089dcdac33cf8ae0b001b
Robert Kirkman <therobertkirkman@gmail.com>
2021-06-23 13:58:58 +0900

    updated textfield

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -3,24 +3,110 @@ import * as React from 'react';
 import { TextField } from '@contentful/forma-36-react-components';
 
 
-const FormaTextField = (props) => (
-  <TextField {...props}>
-    {props.children}
-  </TextField>
-);
-
-
-FormaTextField.propTypes = {
-  disabled: PropTypes.bool,
-  labelText: PropTypes.string,
-  helpText: PropTypes.string,
+function FormaTextField(props) {
+  const { placeholder, maxLength, disabled, ...other } = props;
+  return (
+    <TextField {...other} textInputProps={{ placeholder, maxLength, disabled }}>
+      {props.children}
+    </TextField>
+  )
 };
 
 
+FormaTextField.propTypes = {
+  /**
+  * The text for the label of the input element
+  * */
+  labelText: PropTypes.string,
+
+  /**
+   * Placeholder text for the input element
+   */
+  placeholder: PropTypes.string,
+
+  /** 
+   * The text value for the input element
+   * @uxpinignoreprop
+   * @uxpinpropname input value
+   * */  
+  value: PropTypes.string,
+
+  /** 
+   * The id for the input element
+   * @uxpinignoreprop
+   * @uxpinpropname input id
+   * */  
+  id: PropTypes.string,
+
+  /** 
+   * The form related name for the input element
+   * @uxpinignoreprop
+   * @uxpinpropname input name
+   * */  
+  name: PropTypes.string,
+
+  /**
+   * If checked: The input element requires completion
+   */
+  required: PropTypes.bool,
+
+  /**
+   * If checked: Displays current and max character count
+   */
+  countCharacters: PropTypes.bool,
+
+  /**
+   * If checked: Displays the maximum characters allowed to be entered in the input element
+   * Only works in preview mode
+   */
+  maxLength: PropTypes.number,
+
+  /**
+   * If checked: Changes input element from TEXTFIELD to TEXTAREA
+   */
+  textarea: PropTypes.bool,
+
+  /**
+   * Help text for the input element
+   */
+  helpText: PropTypes.string,
+
+
+  /**
+   * Validation message for the input element
+   */
+  validationMessage: PropTypes.string,
+
+  /**
+   * If checked: The input element will be disabled
+   */
+  disabled: PropTypes.bool,
+
+  /**
+  * onChange function
+  * */
+  onChange: PropTypes.func,
+
+  /**
+  * onBlur function
+  * */
+  onBlur: PropTypes.func,
+
+  /**
+  * onFocus function
+  * */
+  onFocus: PropTypes.func,
+};
+
 
 FormaTextField.defaultProps = {
-  labelText: "Email address",
-  helpText: "Please enter your email address",
+ labelText: "Label Text prop",
+ placeholder: "Placeholder prop",
+ required: true,
+ countCharacters: true,
+ maxLength: 100,
+ helpText: "Help Text prop",
+ validationMessage: "Validation Message prop",
 };
 
 export { FormaTextField as default };
